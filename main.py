deec57ec3b308e2e9d6ba86b4611d2b36bb76f3c
David <davidmartinezmit@gmail.com>
2022-03-11 23:10:42 +0100

    Sockets: client sense comentar

---
+++ main.py
@@ -0,0 +1,35 @@
+import socket
+import sys
+
+c = socket.socket()
+server = ('169.254.242.78', 4444)
+try:
+    c.connect(server)
+except ConnectionError:
+    print("Can't connect, check IP or Connection.")
+    sys.exit()
+print("Send the number of the option you want: distance / ledon / ledoff / exit.")
+
+
+while True:
+    message = input("Write the option: ")
+    c.sendall(message.encode('utf-8'))
+    try:
+        if message == "distance":
+            data, addr = c.recvfrom(1024)
+            data = data.decode('utf-8')
+            print("Distance from hc-sr04: " + data + "cms.")
+        elif message == "ledon":
+            print("Led is on")
+        elif message == "ledoff":
+            print("Led is off")
+        elif message == "exit":
+            print("System shutting down.")
+            break
+        else:
+            print("Wrong message. Must be: distance / ledon / ledoff / exit")
+    except KeyboardInterrupt:
+        c.close()
+        sys.exit()
+
+
