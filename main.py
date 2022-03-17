11e5abd5b77ab362564ad695cc5579f91c7d221d
David <davidmartinezmit@gmail.com>
2022-03-17 10:00:01 +0100

    Sockets: client comentat

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 c = socket.socket()
 server = ('169.254.242.78', 4444)
+# Code to try to connect to the server, if failed, changes the port
 try:
     c.connect(server)
 except ConnectionError:
@@ -13,14 +14,16 @@ except ConnectionError:
     except ConnectionError:
         print("Can't connect. Port 4444 and 4445 are in use. Try again later.")
         sys.exit()
-
+# Prints the possible options for the user
 print("Send the number of the option you want: distance / ledon / ledoff / exit.")
 
-
 while True:
+    # We ask the user to write the command
     message = input("Write the option: ")
+    # We encode the message and send it to the server
     c.sendall(message.encode('utf-8'))
     try:
+        # Menu to show the user the consequence of the action selected
         if message == "distance":
             data, addr = c.recvfrom(1024)
             data = data.decode('utf-8')
